078170a277d27f5dd3a5bf2bb293d0b6b46ec583
hamiguazzz <429556240@qq.com>
2018-07-29 11:10:23 +0800

    :construction:add data source and it's reader

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
-import hamiguazzz.word.helper.WordBuilder;
+import hamiguazzz.database.SQLXMLReader;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		System.out.println(WordBuilder.buildWordFromNet("link"));
+		//System.out.println(WordBuilder.buildWordFromNet("link"));
+		File file = new File("./src/hamiguazzz/word/wordlist.xml");
+		try {
+			SQLXMLReader reader = new SQLXMLReader(file);
+			System.out.println(reader.getColumnsNameMap("words"));
+		} catch (ParserConfigurationException | SAXException e) {
+			e.printStackTrace();
+		}
 	}
 }
